fd8f46ec47a4c7fe31c8cb97330ed0ddfd4c1f10
murilo.aragao <murilo.matos2005@gmail.com>
2024-10-08 11:39:18 -0300

    Initial commit

---
+++ src/main/java/com/example/creditanalisys/CreditAnalisysApplication.java
@@ -0,0 +1,13 @@
+package com.example.creditanalisys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAnalisysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditAnalisysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/creditanalisys/CreditAnalisysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.creditanalisys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAnalisysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
