758ea14a0a559cb7780c66d8e5207cda293de286
Aquila-Almeida <aquilaalvesdealmeida@gmail.com>
2024-10-23 11:57:19 -0300

    add Solicitacao Crédito DTO

--- src/main/java/com/example/creditanalisys/model/dtos/SolCredDTO.java
+++ src/main/java/com/example/creditanalisys/model/dtos/SolCredDTO.java
@@ -1,4 +1,20 @@
 package com.example.creditanalisys.model.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class SolCredDTO {
+    private Long id;
+    private Long user_id;
+    private String status;
+    private BigDecimal valor;
+    private LocalDate data_solicitacao;
 }
