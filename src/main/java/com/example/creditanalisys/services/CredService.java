9f1d557abeb1dd89ceec9043a09acd185e86058a
Aquila-Almeida <aquilaalvesdealmeida@gmail.com>
2024-10-28 10:32:12 -0300

    add delete Solicitaçao Crédito, add get All Solicitaçoes

--- src/main/java/com/example/creditanalisys/services/CredService.java
+++ src/main/java/com/example/creditanalisys/services/CredService.java
@@ -8,7 +8,11 @@ import com.example.creditanalisys.model.entities.User;
 import com.example.creditanalisys.repositories.SoliCredRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -22,5 +26,19 @@ public class CredService {
         SolicitacaoCredito solicitacaoCredito = DozerConverter.parseObject(solCredDTO, SolicitacaoCredito.class);
         SolicitacaoCredito savedSolCred = soliCredRepository.save(solicitacaoCredito);
         return DozerConverter.parseObject(savedSolCred, SolCredDTO.class);
+    }
+    public ResponseEntity<SolCredDTO> deleteSolicitacaoCredito(Long id){
+        Optional<SolicitacaoCredito> solicitacaoCreditoOptional = SoliCredRepository.findById(id);
+        if (solicitacaoCreditoOptional.isPresent()){
+            SolicitacaoCredito solicitacao = solicitacaoCreditoOptional.get();
+            soliCredRepository.delete(solicitacao);
+            return ResponseEntity.ok(DozerConverter.parseObject(solicitacao, SolCredDTO.class));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    public List<SolCredDTO> getAllSolitacoes(){
+        List<SolicitacaoCredito> solicitacoes = soliCredRepository.findAll();
+        return DozerConverter.parseListObjects(solicitacoes, SolCredDTO.class);
     }
 }
