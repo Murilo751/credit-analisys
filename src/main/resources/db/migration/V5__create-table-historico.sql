d5f54f4f433dedb5e72351f499778abdd509462b
Murilo751 <murilo.matos2005@gmail.com>
2024-11-01 19:57:30 -0300

    Add Migrations finish

--- src/main/resources/db/migration/V5__create-table-historico.sql
+++ src/main/resources/db/migration/V5__create-table-historico.sql
@@ -1,9 +1,9 @@
-CREATE TABLE historico (
-                           id SERIAL PRIMARY KEY,
-                           pontuacao INT,
-                           pagamentosPontuais INT,
-                           incidentes INT,
-                           dividas DECIMAL(10, 2),
-                           userId BIGINT,
-                           FOREIGN KEY (userId) REFERENCES users(id)
+CREATE TABLE IF NOT EXISTS historico (
+                                         id SERIAL PRIMARY KEY,
+                                         pontuacao INT,
+                                         pagamentosPontuais INT,
+                                         incidentes INT,
+                                         dividas DECIMAL(10, 2),
+                                         userId BIGINT,
+                                         FOREIGN KEY (userId) REFERENCES users(id) ON DELETE CASCADE
 );
